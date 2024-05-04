121bc0096fefe8db2a5e294cc10bff6746c2c217
Apurba123456 <126500102+Apurba123456@users.noreply.github.com>
2024-05-04 14:36:08 +0000

    Spring meme controller add

---
+++ src/main/java/com/memeworld/memeworld/MemeworldApplication.java
@@ -0,0 +1,13 @@
+package com.memeworld.memeworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeworldApplication.class, args);
+	}
+
+}
